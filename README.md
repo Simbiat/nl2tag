8a809b0707ec2c86fb754772191db03d8e456fdd
Simbiat <simbiat@outlook.com>
2022-11-11 14:40:46 +0200

    Basic nl2p logic

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nl2p
-Stub for `nl2p` class to convert new lines to `<p>` tags
+# nl2tag
+Class to convert new lines to various HTML tags: `br`, `p` and `li`.
+
+## Why?
+Initially the idea was for a version of `nl2br`, but for paragraphs. If you google (or [Stackoverflow](https://stackoverflow.com/questions/3738124/nl2br-for-paragraphs)) for it, you can find several easy approaches for this, but all of those that I found have at least some flaws:
+1. They do not check whether there is already a `p` tag around respective paragraph. This can result in extra paragraphs. At least some browsers will then show this as extra paragraphs, meaning `<p>line1<p>line2</p>line3</p>` will result in 3 paragraphs, which may not be the intention.
+2. In fact, there is a bunch of tags, that are not expected inside of `p`, as per the [spec](https://html.spec.whatwg.org/#phrasing-content) of `phrasing content`. Or rather there is a limited set of tags, tha can be.
+3. They will change new lines inside tags, where you want to preserve new lines as is. `pre` and `textarea` are the ones, where you could generally want that. `code`, `samp`, `kbd` and `var` are an example of other common values, but technically it can be any tag with `white-space` CSS property set to either `pre`, `pre-wrap`, `pre-line` or `break-spaces`.  
+
+When you are working with HTML, these flaws can become an issue, thus I wanted to find a way to circumvent them, hence this library. But at early stage of conceptualization, I realized, that it would make sense to also do something similar for `li` items and even `br`.
+
+`$preservedIn` - list of HTML tags, where you want to preserve new lines as is. 
--- src/nl2tag.php
+++ src/nl2tag.php
@@ -2,7 +2,90 @@
 declare(strict_types=1);
 namespace Simbiat;
 
-class nl2Tag
+use function Twig\Tests\html;
+
+class nl2tag
 {
-    #stub
+    #Common tags, in which you may want to preserve the new lines
+    private array $preserveIn = ['pre', 'textarea', 'code', 'samp', 'kbd', 'var'];
+    #Tags, that are allowed in `p`, except for `area`, `link` and `meta`, may be included under certain conditions.
+    #Add them manually along with any other custom tags, if you know that they can be in the piece of text you are parsing.
+    private array $phrasingContent = ['a', 'abbr', 'audio', 'b', 'bdi', 'bdo', 'br', 'button', 'canvas', 'cite', 'code', 'data', 'datalist',
+                                        'del', 'dfn', 'em', 'embed', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'map', 'mark', 'math', 'meter',
+                                        'noscript', 'object', 'output', 'picture', 'progress', 'q', 'ruby', 's', 'samp', 'script', 'select', 'slot', 'small',
+                                        'span', 'strong', 'sub', 'sup', 'svg', 'template', 'textarea', 'time', 'u', 'var', 'video', 'wbr'];
+    #Original string before any modification
+    private string $originalString = '';
+    #Split string
+    private array $splitString = [];
+    #List of found tags
+    private array $foundTags = ['preserveIn' => ['opening' => [], 'closing' => []], 'notPhrasing' => ['opening' => [], 'closing' => []]];
+    
+    public function __construct(string $string, array $preserveIn = ['pre', 'textarea', 'code', 'samp', 'kbd', 'var'], array $phrasingContent = [])
+    {
+        $this->preserveIn = $preserveIn;
+        if (!empty($phrasingContent)) {
+            $this->phrasingContent = array_merge($this->phrasingContent, $phrasingContent);
+        }
+        $this->originalString = $string;
+        $this->getTags();
+        $this->splitString = $this->split();
+    }
+    
+    public function nl2br(): string
+    {
+        return '';
+    }
+    
+    public function nl2p(): string
+    {
+        $newString = '';
+        foreach ($this->splitString as $part) {
+            #Check if the part is already wrapped in <p>
+            if (preg_match('/^<p[^>]*>.*<\/p>$/uis', $part) === 1) {
+                #Add a string as is
+                $newString .= $part;
+            } else {
+                #Need to check for opening tags, where we preserve spaces
+                
+                #Need to check for tags, that are not allowed in paragraphs. Need to have both opening and closing parts
+                
+                $newString .= '<p>'.$part.'</p>';
+            }
+        }
+        return $newString;
+    }
+    
+    public function nl2li(): string
+    {
+        return '';
+    }
+    
+    #Function to split the string into pieces
+    private function split(): array
+    {
+        $parts = preg_split('/\R/ui', $this->originalString);
+        if (!is_array($parts)) {
+            return [];
+        } else {
+            return $parts;
+        }
+    }
+    
+    #Function to check for certain HTML tags
+    private function getTags(): void
+    {
+        #Check if we have tags, that need new lines preservation. First we get the count of open ones
+        preg_match_all('/<(p|'.implode('|', $this->preserveIn).')[^>]*>/ui', $this->originalString, $foundTags, PREG_PATTERN_ORDER);
+        $this->foundTags['preserveIn']['opening'] = $foundTags[0];
+        #Now closing ones
+        preg_match_all('/<\/(p|'.implode('|', $this->preserveIn).')[^>]*>/ui', $this->originalString, $foundTags, PREG_PATTERN_ORDER);
+        $this->foundTags['preserveIn']['closing'] = $foundTags[0];
+        #Check for tags, which are not phrasing content
+        preg_match_all('/<(?!p|\/|'.implode('|', $this->phrasingContent).')[^>]*>/ui', $this->originalString, $foundTags, PREG_PATTERN_ORDER);
+        $this->foundTags['notPhrasing']['opening'] = $foundTags[0];
+        #Closing tags
+        preg_match_all('/<\/(?!p|'.implode('|', $this->phrasingContent).')[^>]*>/ui', $this->originalString, $foundTags, PREG_PATTERN_ORDER);
+        $this->foundTags['notPhrasing']['opening'] = $foundTags[0];
+    }
 }
